acfcbfce6f2d76d949960f37cdca3ef279cabe88
kiokoEVA <everlyne.kioko@strathmore.edu>
2024-06-04 10:11:53 +0300

    fundamentals of php

---
+++ Personal porfolio website/in.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+echo"This is my my first php"; //Displaying sentence or a string
+
+print "<br>";
+
+print "Today is thursday";
+
+?>
+    
+</body>
+</html>
